995b693707580d42222421d70e48be7fe9ba2952
Rhi4nn3 <sofiarey6309@gmail.com>
2026-07-29 16:33:57 +0800

    color changing shape

---
+++ Activity/Activity6/Activity6.js
@@ -0,0 +1,31 @@
+function changeColor(){
+    document.getElementById("balls").src="red.png";
+}
+
+function changeColor2(){
+    document.getElementById("balls").src="orange.png";
+}
+
+function changeColor3(){
+    document.getElementById("balls").src="yellow.png";
+}
+
+function changeColor4(){
+    document.getElementById("balls").src="green.png";
+}
+
+function changeColor5(){
+    document.getElementById("balls").src="blue.png";
+}
+
+function changeColor6(){
+    document.getElementById("balls").src="indigo.png";
+}
+
+function changeColor7(){
+    document.getElementById("balls").src="violet.png";
+}
+
+function changeColor8(){
+    document.getElementById("balls").src="pink.png";
+}
